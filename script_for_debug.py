a13dbba76bacc957695b7e10a7a7e92a6869d7d8
Vanik <vanik_888@mail.ru>
2014-05-09 01:27:17 +0400

    First commit

---
+++ script_for_debug.py
@@ -0,0 +1,17 @@
+def is_prime_number(value):
+    i = 2
+    is_prime = True
+    while i < value:
+        if value % i == 0:
+            is_prime = False
+            break
+        i = i + 1
+    return is_prime
+
+i = 0
+j = 2
+while i < 10001:
+    if is_prime_number(j):
+        i += 1
+    j+=1
+print("The 10001 prime number is " + str(j-1))
